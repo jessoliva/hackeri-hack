90a3c90587687d58d025813823e632c85100f772
jessoliva <95152563+jessoliva@users.noreply.github.com>
2022-04-21 16:26:35 -0500

    index.js in models completed

---
+++ models/index.js
@@ -0,0 +1,35 @@
+// import all models
+const User = require('./User');
+const Post = require('./Post');
+const Save = require('./Save');
+const Comment = require('./Comment');
+
+// create associations
+// User and Post
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+});
+
+// Comment Model Associations
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Comment };
