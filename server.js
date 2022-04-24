0406dc8b78b6dd1261625546d6e75a0e8ab5b55b
jessoliva <95152563+jessoliva@users.noreply.github.com>
2022-04-24 00:00:42 -0500

    sign up sample complete

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
+const path = require('path');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// use static files in public folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 // turn on routes
 app.use(routes);
